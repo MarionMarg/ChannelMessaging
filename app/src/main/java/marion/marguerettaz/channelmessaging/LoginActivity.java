45983be11e0da14073c1507b6040fe23e418e4cc
marguerm <mel-huez@hotmail.fr>
2017-03-10 11:31:50 +0100

    Petits ajouts

--- app/src/main/java/marion/marguerettaz/channelmessaging/LoginActivity.java
+++ app/src/main/java/marion/marguerettaz/channelmessaging/LoginActivity.java
@@ -1,15 +1,22 @@
 package marion.marguerettaz.channelmessaging;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.gson.Gson;
 
 import java.util.HashMap;
@@ -24,6 +31,11 @@ public class LoginActivity extends Activity implements View.OnClickListener, OnD
     TextView textViewId;
     TextView textViewMdp;
     Button btnValider;
+    ImageView logo;
+    Handler mHandlerTada;
+    Handler mHandlerAnimLeft;
+    int mShortDelay;
+    int mReallyShortDelay;
 
     public static final String PREFS_NAME = "MyPrefsFile";
 
@@ -39,6 +51,22 @@ public class LoginActivity extends Activity implements View.OnClickListener, OnD
         textMdp.setText("marionmarguerettaz");
         btnValider = (Button) findViewById(R.id.valider);
         btnValider.setOnClickListener(this);
+
+
+        logo = (ImageView) findViewById(R.id.imageViewLogo1);
+
+        mHandlerTada = new Handler(); // android.os.handler
+        mShortDelay = 4000; //milliseconds
+
+        mHandlerTada.postDelayed(new Runnable(){
+            public void run(){
+                YoYo.with(Techniques.Tada)
+                        .duration(700)
+                        .repeat(5)
+                        .playOn(findViewById(R.id.imageViewLogo1));
+                mHandlerTada.postDelayed(this, mShortDelay);
+            }
+        }, mShortDelay);
     }
 
 
@@ -70,8 +98,19 @@ public class LoginActivity extends Activity implements View.OnClickListener, OnD
         editor.commit();
         //Pour récupérer les settings.getString("accesstoken","");
         if(connect1.code == 200) {
-            Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
-            startActivity(myIntent);
+            mReallyShortDelay = 300;
+            mHandlerAnimLeft = new Handler();
+            mHandlerAnimLeft.postDelayed(new Runnable(){
+                public void run(){
+                    Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
+                    startActivity(myIntent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, logo, "logo").toBundle());
+                    b
+                }
+            }, mReallyShortDelay);
+
+            Animation animSlideLeft = AnimationUtils.loadAnimation(this, R.anim.slide_left);
+            btnValider.startAnimation(animSlideLeft);
+
         }
         else
             Toast.makeText(LoginActivity.this, "Mauvais identifiants", Toast.LENGTH_SHORT).show();
